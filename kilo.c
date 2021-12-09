62f00bdbe3354490b5c9df7804e13f36c6f06589
David <theddaywork@users.noreply.github.com>
2021-12-09 13:35:04 -0500

    Improve handling of terminal attributes
    Added disableRawMode to restore the terminal's attributes as found at start of program.  Added atexit setting to allow it to be called when kilo exits.

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -6,11 +7,17 @@
  * It's ultimately from antirez's work.
  */
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
-
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
